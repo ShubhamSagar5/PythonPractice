220af1fa84f5149431fad45c295943a8eb827e33
shubham <shubhamsagar628@gmail.com>
2025-07-28 17:01:54 +0530

    Done

--- Practice/01.py
+++ Practice/01.py
@@ -1,1 +1,43 @@
-print("hello jee")
+#print("hello jee") 
+
+#-----------------------------------------------------------------------------
+# a = int(input("Enter first number : ")) 
+# b = int(input("Enter sec number: ")) 
+
+# print(a+b) 
+
+#----------------------------------------------------------------------------- 
+
+# num = input("Enter number")
+
+# print(type(num)) 
+
+#----------------------------------------------------------------------------- 
+ 
+# a = int(input("Enter first number : ")) 
+
+# b = int(input("Enter second number : ")) 
+
+# c = (a+b) /2
+
+# print("the avg of this two number :",type(c)) 
+
+#----------------------------------------------------------------------------- 
+
+# a = int(input("Enter number that you want to make square : ")) 
+# ans  = a**3 
+
+# print("Ans:",ans) 
+
+#-----------------------------------------------------------------------------  
+
+# str = "01234567890" 
+
+# strSlice = str[-3:-1]
+# print(strSlice) 
+
+#-----------------------------------------------------------------------------  
+
+
+str = "This is good boy \t boy "
+print(str) 
