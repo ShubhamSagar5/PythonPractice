89607e5c33dec2b0ad969bb3eda62cc04f70727f
shubham <shubhamsagar628@gmail.com>
2025-07-31 11:24:19 +0530

    Chapter five Done

--- Practice/01.py
+++ Practice/01.py
@@ -39,5 +39,146 @@
 #-----------------------------------------------------------------------------  
 
 
-str = "This is good boy \t boy "
-print(str) 
+# str = "This is good boy \t boy "
+# print(str)  
+
+#-----------------------------------------------------------------------------
+
+# a = int(input("Enter number "))
+
+
+# print("The square of two number is : ",a**2) 
+
+#----------------------------------------------------------------------------- 
+# letter = "Dear Harry, \n this python course is nice. \n Thanks!" 
+
+# print(letter) 
+
+#----------------------------------------------------------------------------- 
+#list is mutable and string is immutable
+# fruit2 = ["mmmm"]
+# fruit = ['apple','apple','mango','orange','strawbeery','coconut','lichy','babana']
+# res = fruit
+# print(res) 
+
+#----------------------------------------------------------------------------- 
+
+# mark = [] 
+
+# student1 = int(input("Enter the mark of first student1")) 
+# mark.append(student1)
+# student2 = int(input("Enter the mark of first student2")) 
+# mark.append(student2)
+# student3 = int(input("Enter the mark of first student3"))
+# mark.append(student3)
+# student4 = int(input("Enter the mark of first student4"))
+# mark.append(student4)
+# student5 = int(input("Enter the mark of first student5"))
+# mark.append(student5)
+# student6 = int(input("Enter the mark of first student6"))
+# mark.append(student6)
+
+# mark.sort() 
+
+# print(mark) 
+
+#----------------------------------------------------------------------------- 
+
+# a = (7, 0, 8, 0, 0, 9) 
+
+# res = sum(a)
+
+# print(res)
+#-----------------------------------------------------------------------------
+
+# lang = {
+#     "billi":"cat",
+#     "kutta":"dog",
+#     "bail":"ox",
+#     "gay":"cow"
+# } 
+
+# word = input("Enter the name of animal that ypu want to see english mean : ") 
+
+
+# print(lang.get(word)) 
+
+#-----------------------------------------------------------------------------
+
+# setNumer = set() 
+# num1 = input("Enter first number")
+# setNumer.add(num1)
+# num2 = input("Enter second number")
+# setNumer.add(num2) 
+# num3 = input("Enter the third number") 
+# setNumer.add(num3) 
+# num4 = input("Enter fourth number") 
+# setNumer.add(num4)
+# num5 = input("Enter five number") 
+# setNumer.add(num5)
+# num6 = input("Enter six number") 
+# setNumer.add(num6)
+# num7 = input("Enter 7 number") 
+# setNumer.add(num7)
+# num8 = input("Enter 8 number") 
+# setNumer.add(num8)
+
+# print(setNumer) 
+
+#----------------------------------------------------------------------------- 
+
+# setNum = {'18',18}
+
+# print(setNum) 
+
+#----------------------------------------------------------------------------- 
+
+# s= { }
+
+# print(type(s)) 
+
+#----------------------------------------------------------------------------- 
+
+# langObj = {
+
+# }
+
+# name = input("Enter the name ")
+# lang = input("Enter your fav lang") 
+
+# langObj.update({name:lang})
+
+# name = input("Enter the name ")
+# lang = input("Enter your fav lang") 
+
+# langObj.update({name:lang})
+
+# name = input("Enter the name ")
+# lang = input("Enter your fav lang") 
+
+# langObj.update({name:lang})
+
+# name = input("Enter the name ")
+# lang = input("Enter your fav lang") 
+
+# langObj.update({name:lang})
+
+# print(langObj)  
+
+#----------------------------------------------------------------------------- 
+
+# s = {8,7,12,'harry',[1,2]} 
+
+# s.update({})
+#----------------------------------------------------------------------------- 
+
+# '''Write a program to print yes when the age entered by the user is greater 
+# than or equal to 18.'''
+
+
+# age  = int(input("Enter your age "))
+
+# if(age >= 18):
+#     print("You are eligible for voting") 
+# else:
+#     print("You are not eligible ")
